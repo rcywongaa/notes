868de955876130763faa7758d15408068bcbbd8f
Rufus Wong <rcywongaa@gmail.com>
2019-02-13 12:03:51 +0800

    Add script for starting vnc with vino

---
+++ start_display.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export DISPLAY=:1
+Xvfb :1 -screen 0 1024x768x16 &
+sleep 1
+
+#/etc/X11/xinit/xinitrc &
+#/etc/X11/Xsession &
+#exec gnome-session
+exec lightdm-session &
+#sudo service lightdm restart
---
+++ start_vino.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export DISPLAY=:1
+gsettings set org.gnome.Vino enabled true
+gsettings set org.gnome.Vino prompt-enabled false
+gsettings set org.gnome.Vino require-encryption false
+sleep 1
+/usr/lib/vino/vino-server
