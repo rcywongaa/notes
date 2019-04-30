446ecc2b8ecd30515157742b396a062d3e67c7d9
Rufus Wong <rcywongaa@gmail.com>
2019-04-30 17:56:46 +0800

    Add script for authorizing ssh

---
+++ authorize_ssh.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cat ~/.ssh/id_rsa.pub | ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "${1}"@"${2}" "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat >>  ~/.ssh/authorized_keys"
