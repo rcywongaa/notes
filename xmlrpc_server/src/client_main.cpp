05db7bd10679b350e71c2f3d46593c75953f59ea
Rufus Wong <rcywongaa@gmail.com>
2019-08-28 15:00:13 +0800

    Add xmlrpc_server client example

--- xmlrpc_server/src/client_main.cpp
+++ xmlrpc_server/src/client_main.cpp
@@ -6,14 +6,21 @@ int main(int argc, char** argv)
     {
         xmlrpc_c::clientSimple client;
         xmlrpc_c::value result;
-        client.call("http://localhost:8080/RPC2", "sample.bleh", "n", &result);
+        try
+        {
+            client.call("http://192.168.88.51:8195/RPC2", "bypassSafety", "n", &result);
+        }
+        catch (girerr::error e)
+        {
+            printf("Failed to call method\n");
+        }
     }
 
-    {
-        xmlrpc_c::clientSimple client;
-        xmlrpc_c::value result;
-        client.call("http://localhost:8080/RPC2", "sample.add", "ii", &result, 5, 7);
-        int const sum((xmlrpc_c::value_int(result)));
-        printf("Result of RPC (sum of 5 and 7): %d\n", sum);
-    }
+    //{
+        //xmlrpc_c::clientSimple client;
+        //xmlrpc_c::value result;
+        //client.call("http://localhost:8080/RPC2", "sample.add", "ii", &result, 5, 7);
+        //int const sum((xmlrpc_c::value_int(result)));
+        //printf("Result of RPC (sum of 5 and 7): %d\n", sum);
+    //}
 }
