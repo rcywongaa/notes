e68ca607c2b7c4c102341cb3ae0667ea7e2c571a
Rufus Wong <rcywongaa@gmail.com>
2019-11-27 11:24:58 +0800

    grep_bag : Enable grepping multiple bagfiles

--- grep_bag.py
+++ grep_bag.py
@@ -6,20 +6,18 @@ import argparse
 import re
 
 parser = argparse.ArgumentParser(description="Greps /rosout for a specific message and reports its timestamps")
-parser.add_argument("bagfile", nargs=1, help="input bag file")
-parser.add_argument("regex", nargs=1, help="regex pattern to search for")
+parser.add_argument("regex", help="regex pattern to search for")
+parser.add_argument("bagfiles", nargs='+', help="input bag file")
 args = parser.parse_args()
 
-bagfile = args.bagfile[0]
-regex = args.regex[0]
+bagfiles = args.bagfiles
+regex = args.regex
 
-info_dict = yaml.load(rosbag.Bag(bagfile, 'r')._get_yaml_info())
-
-# import pdb
-
-for topic, msg, t in rosbag.Bag(bagfile).read_messages():
-    if topic == "rosout":
-        if re.search(regex, msg.msg) or re.search(regex, msg.name):
-            seconds_from_start = t.to_sec() - info_dict["start"]
-            print("[" + str(seconds_from_start) + "] " + msg.msg)
-            # pdb.set_trace()
+for bagfile in bagfiles:
+    print("From " + str(bagfile) + ":")
+    info_dict = yaml.load(rosbag.Bag(bagfile, 'r')._get_yaml_info())
+    for topic, msg, t in rosbag.Bag(bagfile).read_messages():
+        if topic == "rosout":
+            if re.search(regex, msg.msg) or re.search(regex, msg.name):
+                seconds_from_start = t.to_sec() - info_dict["start"]
+                print("[" + str(seconds_from_start) + "] " + msg.msg)
